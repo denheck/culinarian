e675a415d849533510b81dfa828732e709104e3b
Dennis Heckman <dennis.heckman@vertive.com>
2014-02-22 16:59:08 -0600

    remove cookbooks in favor of librarian-chef

--- cookbooks/plex-server/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             "plex-server"
-maintainer       "Dennis Heckman"
-maintainer_email "denheck@gmail.com"
-license          "Apache 2.0"
-description      "Installs and configures Plex media server"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
-
-supports "ubuntu"
